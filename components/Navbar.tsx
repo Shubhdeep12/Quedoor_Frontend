1653e6bc6ff9c00316b3bc2f8fbb0ff3fb3ff565
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2023-10-11 09:24:59 +0530

    navbar fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,32 +24,39 @@ export default function Navbar() {
 		<div className='w-full py-0.5 shadow-sm bg-white'>
 			<div className='flex px-8 gap-8 h-[60px] items-center max-w-laptop mx-auto'>
 				<QuedoorLogoIcon size={60} className='cursor-pointer' onClick={() => router.push('/feed')} />
-				<div className='flex flex-1 justify-between items-center hover:bg-slate-50 cursor-pointer transition-all border border-neutral-200 rounded-full py-3 px-5'>
+
+				<button className='flex flex-1 justify-between items-center hover:bg-slate-50 cursor-pointer transition-all border border-neutral-200 rounded-full py-3 px-5'>
 					<Text className='text-sm text-neutral-500'>Search post using image, text, etc.</Text>
 					<LuSearch size={16} />
-				</div>
+				</button>
+
 				<div className='w-[1px] h-full border' />
+
 				<div className='flex gap-8 items-center'>
-					<div className='flex flex-col p-2 gap-0.5 items-center cursor-pointer' onClick={() => router.push('/feed')}>
+					<button
+						className='flex flex-col p-2 gap-0.5 items-center cursor-pointer'
+						onClick={() => router.push('/feed')}
+					>
 						{pathname.startsWith('/feed') ? <FeedFilled className='fill-orange-400' /> : <FeedOutlined />}
 						<Text className='text-sm text-neutral-500'>Feed</Text>
-					</div>
-					<div
+					</button>
+					<button
 						className='flex flex-col p-2 gap-0.5 items-center cursor-pointer'
 						onClick={() => router.push('/profile')}
 					>
 						{pathname.startsWith('/profile') ? <AvatarFilled className='fill-orange-400' /> : <AvatarOutlined />}
 						<Text className='text-sm text-neutral-500'>Profile</Text>
-					</div>
+					</button>
 				</div>
+
 				<div className='w-[1px] h-full border' />
 
 				<Popover>
 					<PopoverTrigger>
-						<div className='flex items-center gap-1 cursor-pointer'>
+						<button className='flex items-center gap-1 cursor-pointer'>
 							<Avatar bg='red.500' size='sm' name={user?.name} src={user?.profileImg} />
 							<GoChevronDown size={16} />
-						</div>
+						</button>
 					</PopoverTrigger>
 					<PopoverContent className='w-[250px] overflow-hidden'>
 						<PopoverArrow />
@@ -58,17 +65,23 @@ export default function Navbar() {
 								<div className='flex flex-col gap-1 items-center px-4 py-5'>
 									<Avatar size='xl' name={user?.name} src={user?.profileImg} />
 									<div className='flex flex-col gap-0.5 items-center'>
-										<Text className='font-bold text-sm'>Shubhdeep Chhabra</Text>
-										<Text className='text-xs'>chhabrashubhdeep@gmail.com</Text>
+										<Text className='font-bold text-sm'>
+											{/* {user?.name} */}
+											Shubhdeep Chhabra
+										</Text>
+										<Text className='text-xs'>
+											{/* {user?.email} */}
+											chhabrashubhdeep@gmail.com
+										</Text>
 									</div>
 								</div>
 								<Button variant='outline' rounded='full' className='w-fit'>
 									<Text className='font-bold'>Go to my Profile</Text>
 								</Button>
 							</div>
-							<div className='px-4 py-3 flex gap-1 items-center cursor-pointer'>
+							<button className='px-4 py-3 flex gap-1 items-center cursor-pointer'>
 								<RiLogoutCircleRLine /> <Text>Sign out</Text>
-							</div>
+							</button>
 						</PopoverBody>
 					</PopoverContent>
 				</Popover>
--- providers/index.tsx
+++ providers/index.tsx
@@ -2,16 +2,12 @@
 
 import { type PropsWithChildren } from 'react';
 import AuthProvider from './AuthProvider';
-import ToastProvider from './ToastProvider';
-import 'react-toastify/dist/ReactToastify.css'; //add this line
 import ChakraUIProvider from './ChakraUIProvider';
 
 const Providers = (props: PropsWithChildren) => {
 	return (
 		<AuthProvider>
-			<ToastProvider>
-				<ChakraUIProvider>{props.children}</ChakraUIProvider>
-			</ToastProvider>
+			<ChakraUIProvider>{props.children}</ChakraUIProvider>
 		</AuthProvider>
 	);
 };
